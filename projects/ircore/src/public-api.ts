75f297762ce0da272b8496520291a64499bd4d5f
Alexander Eberle <alexander171294@gmail.com>
2021-09-27 23:19:45 -0300

    deleting v2

--- projects/ircore/src/public-api.ts
+++ projects/ircore/src/public-api.ts
@@ -2,65 +2,28 @@
  * Public API Surface of ircore
  */
 
-/** Main classes */
-export * from './lib/IRCParserV2';
-export * from './lib/ircore.module';
-export * from './lib/IRCore.service';
+/** core **/
+export * from './lib/v3/core/custom.websocket'
+export * from './lib/v3/core/IRCParserV3'
+export * from './lib/v3/core/ModeParser'
+export * from './lib/v3/core/server.data'
 
-/** SERVICES */
-export * from './lib/services/ChannelData';
-export * from './lib/services/channels.service';
-export * from './lib/services/privmsg.service';
-export * from './lib/services/PrivmsgData';
-export * from './lib/services/server-msg.service';
-export * from './lib/services/user-info.service';
-export * from './lib/services/who-status.service';
+/** DOMAINS */
+export * from './lib/v3/domain/channelChat'
+export * from './lib/v3/domain/message'
+export * from './lib/v3/domain/privChat'
+export * from './lib/v3/domain/rawMessage'
+export * from './lib/v3/domain/userData'
 
-/** HANDLERS */
-export * from './lib/handlers/Away.handler';
-export * from './lib/handlers/ChannelList.handler';
-export * from './lib/handlers/ChannelStatus.handler';
-export * from './lib/handlers/Gmode.handler';
-export * from './lib/handlers/Ignore.Handler';
-export * from './lib/handlers/Join.handler';
-export * from './lib/handlers/Kick.handler';
-export * from './lib/handlers/List.handler';
-export * from './lib/handlers/Message.handler';
-export * from './lib/handlers/Mode.handler';
-export * from './lib/handlers/Moderated.handler';
-export * from './lib/handlers/Motd.handler';
-export * from './lib/handlers/Part.handler';
-export * from './lib/handlers/Quit.handler';
-export * from './lib/handlers/Server.handler';
-export * from './lib/handlers/Status.handler';
-export * from './lib/handlers/Users.handler';
-export * from './lib/handlers/Who.handler';
-export * from './lib/handlers/Whois.handler';
-
-/** DTOs */
-export * from './lib/dto/Away';
-export * from './lib/dto/Channel';
-export * from './lib/dto/ChannelInfo';
-export * from './lib/dto/IndividualMessage';
-export * from './lib/dto/Join';
-export * from './lib/dto/KickInfo';
-export * from './lib/dto/NewMode';
-export * from './lib/dto/NickChange';
-export * from './lib/dto/Part';
-export * from './lib/dto/Quit';
-export * from './lib/dto/User';
-export * from './lib/dto/UserInChannel';
-export * from './lib/dto/Who';
-export * from './lib/dto/WhoIs';
+/** SERVICES **/
+export * from './lib/v3/services/channels.service'
+export * from './lib/v3/services/glob-user.service'
+export * from './lib/v3/services/notices.service'
+export * from './lib/v3/services/privs.service'
+export * from './lib/v3/services/server.service'
 
 /** UTILS */
 export * from './lib/utils/EmoteList';
-export * from './lib/utils/IRCMessage.util';
 export * from './lib/utils/PostProcessor';
 export * from './lib/utils/Time.util';
-export * from './lib/utils/UModes.utils';
-export * from './lib/utils/validRegex';
-export * from './lib/utils/WebSocket.util';
 
-/** Helpers */
-export * from './lib/helpers/avatar.helper';
